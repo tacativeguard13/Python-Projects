a3a3f0929a7e21a9b7a2f57ceedaa959d258575c
tacativeguard13 <87353368+tacativeguard13@users.noreply.github.com>
2022-06-19 12:29:46 +0530

    Commit 2

--- multi_avg_finder.py
+++ multi_avg_finder.py
@@ -8,7 +8,7 @@ n2 = int(n2)
 n3 = int(n3)
 n4 = int(n4)
 
-avg1 = (n1 + n2 + n3 + n4)
-avg2 = (avg1 /4)
+avg1 = ((n1 + n2 + n3 + n4)/4)
 
-print("the avrege of the given numbers is" , avg2)
+
+print("the avrege of the given numbers is" , avg1)
