8c45e960da830026f78f333a18f29bbe09a2b94a
tacativeguard13 <87353368+tacativeguard13@users.noreply.github.com>
2022-06-19 12:26:11 +0530

    first commit

---
+++ multi_avg_finder.py
@@ -0,0 +1,14 @@
+n1 = input("Enter your first number : ")
+n2 = input("Enter your second nunmber : ")
+n3 = input("Enter your third nunmber : ")
+n4 = input("Enter your fourth nunmber : ")
+
+n1 = int(n1)
+n2 = int(n2)
+n3 = int(n3)
+n4 = int(n4)
+
+avg1 = (n1 + n2 + n3 + n4)
+avg2 = (avg1 /4)
+
+print("the avrege of the given numbers is" , avg2)
---
+++ square_finder.py
@@ -0,0 +1,7 @@
+number = input("Enter your number : ")
+
+number = int(number)
+
+square = (number*number)
+
+print ("The square of" , number , "is" , square)
