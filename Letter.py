85ffdb112878707405733bf3e3b3f81772dcf727
tacativeguard13 <87353368+tacativeguard13@users.noreply.github.com>
2022-06-19 20:15:19 +0530

    Commit 1

--- Letter.py
+++ Letter.py
@@ -9,6 +9,8 @@ Date <|Date|>
 '''
 name = input("Enter Your name\n")
 date = input('''Enter Date\n''')
+
 letter = letter.replace("<|name|>" , name)
 letter = letter.replace("<|Date|>" , date)
+
 print(letter)
